94805fa6f16ccbfeca25353e0157cc9dbdb5a4cb
Nico Hofmann <nico.hofmann1998@gmx.de>
2024-03-01 15:07:12 +0100

    GetMetaData relocated to ID3Servie

--- Song_Bibliothek/ID3Service.cs
+++ Song_Bibliothek/ID3Service.cs
@@ -1,8 +1,76 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Song_Bibliothek.Pages.Album;
+using Song_Bibliothek.Pages.Artists;
+using Song_Bibliothek.Pages.Songs;
+
 namespace Song_Bibliothek
 {
-    public interface ID3Service
+    public class ID3Service
     {
-        void GetMetaData();
+        public SongInfo GetMetaDataSong(HttpRequest request, SongInfo songInfo)
+        {
+            try
+            {
+                //Recieve request
+                var mp3File = request.Form.Files["file"];
+
+                string filePath = "";
+
+                if (mp3File == null)
+                {
+                    throw new FileNotFoundException();
+                }
+
+                //Create file for server to save
+                filePath = Path.Combine(Path.Combine(Directory.GetCurrentDirectory(), "AudioFiles"), mp3File.FileName);
+
+                songInfo.fileFormat = filePath;
+
+
+                //Get Metadata from file
+                var metaData = TagLib.File.Create(filePath);
+
+                songInfo.title = metaData.Tag.Title ?? "Unknown";
+                songInfo.artist = metaData.Tag.AlbumArtists.FirstOrDefault("Unknown");
+                songInfo.album = metaData.Tag.Album ?? "Unknown";
+                songInfo.year = metaData.Tag.Year.ToString() ?? "Unknown";
+                songInfo.track = metaData.Tag.Track.ToString() ?? "Unknown";
+
+                string replaceTitle = songInfo.title.Replace("'", "´");
+                string replaceAlbum = songInfo.album.Replace("'", "´");
+                string replaceLyrics = songInfo.lyrics.Replace("'", "´");
+                string replaceArtist = songInfo.artist.Replace("'", "´");
+                string replaceFileFormat = songInfo.fileFormat.Replace("'", "´");
+
+                songInfo.title = replaceTitle;
+                songInfo.artist = replaceArtist;
+                songInfo.album = replaceAlbum;
+                songInfo.lyrics = replaceLyrics;
+                songInfo.fileFormat = replaceFileFormat;
+
+                //Copy file for application to use
+                using (var fileStreamCreate = new FileStream(songInfo.fileFormat, FileMode.Create))
+                {
+                    mp3File.CopyTo(fileStreamCreate);
+                }
+
+                if (mp3File == null || mp3File.Length == 0)
+                    throw new FileNotFoundException();
+
+                //Convert to byte to save in database
+                var fileStreamOpen = new FileStream(songInfo.fileFormat, FileMode.Open);
+
+                songInfo.data = new byte[fileStreamOpen.Length];
+
+                fileStreamOpen.Read(songInfo.data, 0, (int)fileStreamOpen.Length);
+
+                return songInfo;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
     }
 }
--- Song_Bibliothek/Pages/Songs/Create.cshtml.cs
+++ Song_Bibliothek/Pages/Songs/Create.cshtml.cs
@@ -5,12 +5,10 @@ using Song_Bibliothek.Pages.Artists;
 
 namespace Song_Bibliothek.Pages.Songs
 {
-    public class CreateModel : PageModel, ID3Service
+    public class CreateModel : PageModel
     {
-
+        private ID3Service? _id3Service;
         public SongInfo songInfo = new SongInfo();
-        public ArtistInfo artistInfo = new ArtistInfo();
-        public AlbumInfo albumInfo = new AlbumInfo();
         public string errorMessage = "";
         public string successMessage = "";
         
@@ -18,11 +16,13 @@ namespace Song_Bibliothek.Pages.Songs
         {
         }
 
-        public void OnPost() 
+        public void OnPost()
         {
             songInfo.lyrics = Request.Form["lyrics"];
-            
-            GetMetaData();
+
+            _id3Service = new ID3Service();
+
+            _id3Service.GetMetaDataSong(Request, songInfo);
 
             try
             {
@@ -36,19 +36,19 @@ namespace Song_Bibliothek.Pages.Songs
                     }
 
                     string sql = "INSERT INTO songs (album_id, song_title, track, lyrics, file_format, data)" +
-                        "VALUES((SELECT album_id FROM album WHERE album_title = '" + songInfo.album + "'), '" + 
+                        "VALUES((SELECT album_id FROM album WHERE album_title = '" + songInfo.album + "'), '" +
                         songInfo.title + "', '" + songInfo.track + "', '" +
                         songInfo.lyrics + "', '" + songInfo.fileFormat + "', @data)";
 
                     int songCount = 0;
-                    string stmt = "SELECT COUNT(*) FROM songs Where song_title = '" + songInfo.title + "'"; 
+                    string stmt = "SELECT COUNT(*) FROM songs Where song_title = '" + songInfo.title + "'";
 
                     using (MySqlCommand cmd = new MySqlCommand(stmt, connection))
                     {
                         songCount = Convert.ToInt32(cmd.ExecuteScalar());
                     }
 
-                    if (songCount >= 0)
+                    if (songCount > 0)
                     {
                         successMessage = "Song already in List";
 
@@ -71,8 +71,8 @@ namespace Song_Bibliothek.Pages.Songs
                     }
                 }
             }
-            catch(Exception ex)
-            { 
+            catch (Exception ex)
+            {
                 errorMessage = ex.Message;
                 return;
             }
@@ -86,71 +86,6 @@ namespace Song_Bibliothek.Pages.Songs
             successMessage = "New Song added";
 
             Response.Redirect("/Songs/Index");
-        }
-
-        public void GetMetaData()
-        {
-            //Recieve request
-            var mp3File = Request.Form.Files["file"];
-
-            string filePath = "";
-
-            if (mp3File == null)
-            {
-                NotFound();
-                return;
-            }
-
-            //Create file for server to save
-            filePath = Path.Combine(Path.Combine(Directory.GetCurrentDirectory(), "AudioFiles"), mp3File.FileName);
-
-            songInfo.fileFormat = filePath;
-
-
-            //Get Metadata from file
-            var metaData = TagLib.File.Create(filePath);
-
-            songInfo.title = metaData.Tag.Title ?? "Unknown";
-            songInfo.artist = metaData.Tag.AlbumArtists.FirstOrDefault("Unknown");
-            songInfo.album = metaData.Tag.Album ?? "Unknown";
-            songInfo.year = metaData.Tag.Year.ToString() ?? "Unknown";
-            songInfo.track = metaData.Tag.Track.ToString() ?? "Unknown";
-
-            string replaceTitle = songInfo.title.Replace("'", "´");
-            string replaceAlbum = songInfo.album.Replace("'", "´");
-            string replaceLyrics = songInfo.lyrics.Replace("'", "´");
-            string replaceArtist = songInfo.artist.Replace("'", "´");
-            string replaceFileFormat = songInfo.fileFormat.Replace("'", "´");
-
-            songInfo.title = replaceTitle;
-            songInfo.artist = replaceArtist;
-            songInfo.album = replaceAlbum;
-            songInfo.lyrics = replaceLyrics;
-            songInfo.fileFormat = replaceFileFormat;
-
-            try
-            {
-                //Copy file for application to use
-                using (var fileStreamCreate = new FileStream(songInfo.fileFormat, FileMode.Create))
-                {
-                    mp3File.CopyTo(fileStreamCreate);
-                }
-
-                if (mp3File == null || mp3File.Length == 0)
-                    return;
-
-                //Convert to byte to save in database
-                var fileStreamOpen = new FileStream(songInfo.fileFormat, FileMode.Open);
-
-                songInfo.data = new byte[fileStreamOpen.Length];
-
-                fileStreamOpen.Read(songInfo.data, 0, (int)fileStreamOpen.Length);
-
-            }
-            catch (Exception ex)
-            {
-                NotFound(ex.Message);
-            }
         }
     }
 }
